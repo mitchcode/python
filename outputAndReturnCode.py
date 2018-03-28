850afd60aa18a023f33a447176160ae5fc60359e
mitchcode <mitchcode1985@gmail.com>
2018-03-27 21:01:55 -0700

    Update outputAndReturnCode.py

--- outputAndReturnCode.py
+++ outputAndReturnCode.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import subprocess
+
 # This short script illustrates a clean way to capture the output of a shell
 # subprocess command AND the return code. The output will not be formatted 
 # properly unless it is decoded. Note the ".decode('ascii')" below.
@@ -25,3 +27,19 @@ returncode = out.returncode
 
 print(output)
 print("Return Code: " + str(returncode))
+
+# In fact, this can be packaged nicely into a function:
+def elegant_shell_execute(command):
+    print("Executing in shell: " + command)
+    out = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    output = out.communicate()[0].decode('ascii')
+    returncode = out.returncode
+    print(output)
+    print("Return Code: " + str(returncode))
+    print("")
+    pass
+
+command = "ls -l /path/to/some/symlink"
+
+elegant_shell_execute(command)
